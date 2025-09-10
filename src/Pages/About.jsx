77bd01aeb042ac70e5c4c66e77f524f70612c3d9
pihu <gouravnandanwar400@gmail.com>
2025-09-10 18:21:43 +0530

    add para

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,9 @@ function About(){
     return(
        <div className="container">
          <h1>About Page</h1>
+         <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa est doloribus libero ut culpa aspernatur officia quibusdam ab commodi aut. 
+         </p>
        </div>
     )
 }
