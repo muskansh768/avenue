5b337a853cc7d6875c43cd8274e76778a24a9509
pihu <gouravnandanwar400@gmail.com>
2025-09-13 18:09:58 +0530

    Footer and code

--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,8 +1,26 @@
 function Service(){
     return(
-       <div className="container">
-         <h1>Service Page</h1>
-       </div>
+       <>
+        <div className="last-img">
+                  <div className="d-flex gap-3">
+                    <div className="last-image">
+                      <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/06/client-logo-nokia-1.png" alt="" className="w-100" />
+                    </div>
+                    <div className="last-image">
+                      <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/06/client-logo-gopro-1.png" alt="" className="w-100" />
+                    </div>
+                    <div className="last-image">
+                      <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/06/client-logo-amd-1.png" alt="" className="w-100" />
+                    </div>
+                    <div className="last-image">
+                      <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/06/client-logo-minagod.png" alt="" className="w-100" />
+                    </div>
+                    <div className="last-image">
+                      <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/06/client-logo-dexon.png" alt="" className="w-100" />
+                    </div>
+                  </div>
+                </div>
+       </>
     )
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,48 +2,187 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer(){
     return(
-        <footer className='bg-blue text-light p-2'>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-3">
-                        <ul> 
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Service</a></li>
-                            <li><a href="#">Career</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Service</a></li>
-                            <li><a href="#">Career</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Service</a></li>
-                            <li><a href="#">Career</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Service</a></li>
-                            <li><a href="#">Career</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div>
+      <footer className=" text-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 p-3">
+              <div className="ftr-hd">
+                <h1>SaaSoft Site</h1>
+              </div>
+              <div className="ftr-pr">
+                <p>
+                  Saasoft specializes in offering software development services that use cutting-edge technological solutions to assist organizations operations.
+                </p>
+              </div>
+              <div className="ftr-input mt-3">
+                <input type="email" name="" id="" />
+              </div>
+              <div className="form-check mt-3">
+                <input className="form-check-input" type="checkbox" value="" id="checkDefault" />
+                <label className="form-check-label" for="checkDefault">
+                  I agree with the terms & conditions
+                </label>
+              </div>
+              <div className="ftr-last mt-3">
+                <div className="d-flex gap-3  align-itmes-center">
+                  <div className="ftr-name align-itmes-center justify-content-center">
+                    <h4>Share:</h4>
+                  </div>
+                  <div className="ftr-icon align-itmes-center justify-content-center">
+                    <i className="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-instagram"></i>
+                    <i className="fa-brands fa-linkedin"></i>
+                    <i className="fa-brands fa-square-twitter"></i>
+                  </div>
                 </div>
+              </div>
             </div>
-        </footer>
+            <div className="col-md-2 p-3">
+              <div className="ftr-hd">
+                <h4>Company</h4>
+              </div>
+              <div className="ftr-tl">
+                <ul className="list-unstyled ">
+                  <li><a href="#" className="text-light text-decoration-none">About us</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Careers</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Our Blog</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Contact us</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Remote Work</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">version Control</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Project Task</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-2 p-3">
+              <div className="ftr-hd">
+                <h4>Product</h4>
+              </div>
+              <div className="ftr-tl">
+                <ul className="list-unstyled ">
+                  <li><a href="#" className="text-light text-decoration-none">Integration</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Customers</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Pricing</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Help Center</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Backlog</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Typetalk</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Case Studies</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Download</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-2 p-3">
+              <div className="ftr-hd">
+                <h4>Legal</h4>
+              </div>
+              <div className="ftr-tl">
+                <ul className="list-unstyled ">
+                  <li><a href="#" className="text-light text-decoration-none">Terms of Use</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Privacy Policy</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Cookie Policy</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Site Map</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">API Developers</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Press Release</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Agencies & Freelancers</a></li>
+                  <li><a href="#" className="text-light text-decoration-none">Partner Program</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-2 p-3">
+              <div className="ftr-hd">
+                <h4>Integration</h4>
+              </div>
+              <div className="ftr-tl">
+                <div className="d-flex  gap-3 ">
+                  <div className="ftr-icon">
+                    <i className="fa-brands fa-github"></i>
+                  </div>
+                  <div className="ftr-ul">
+                    <ul className="list-unstyled ">
+                      <li><a href="#" className="text-light text-decoration-none">Github</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="ftr-tl">
+                <div className="d-flex  gap-3 ">
+                  <div className="ftr-icon">
+                    <i className="fa-brands fa-discord" style={{ 'color': '#B197FC' }}></i>
+                  </div>
+                  <div className="ftr-ul">
+                    <ul className="list-unstyled ">
+                      <li><a href="#" className="text-light text-decoration-none">Discord</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="ftr-tl">
+                <div className="d-flex  gap-3 ">
+                  <div className="ftr-icon">
+                    <i className="fa-brands fa-google-play" style={{ 'color': '#B197FC' }}></i>
+                  </div>
+                  <div className="ftr-ul">
+                    <ul className="list-unstyled ">
+                      <li><a href="#" className="text-light text-decoration-none">Google Play</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="ftr-tl">
+                <div className="d-flex  gap-3 ">
+                  <div className="ftr-icon">
+                    <i className="fa-brands fa-instagram" style={{ 'color': '#B197FC' }}></i>
+                  </div>
+                  <div className="ftr-ul">
+                    <ul className="list-unstyled ">
+                      <li><a href="#" className="text-light text-decoration-none">Instagram</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="ftr-tl">
+                <div className="d-flex  gap-3 ">
+                  <div className="ftr-icon">
+                    <i className="fa-brands fa-facebook" style={{ 'color': '#B197FC' }}></i>
+                  </div>
+                  <div className="ftr-ul">
+                    <ul className="list-unstyled ">
+                      <li><a href="#" className="text-light text-decoration-none">Facebook</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-md-4">
+              <div className="ftr-pr">
+                <p>@SaaSoft 2024 All Rights Reserved</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="ftr-img">
+                <img src="https://saasoft.wpengine.com/wp-content/uploads/2024/05/payment-methods.png" alt="" className="w-100" />
+              </div>
+            </div>
+            <div className="col-md-4 text-end">
+              <div className="ftr-tl">
+                <div className="d-flex gap-3 justify-content-end">
+                  <div className="ft-tl">
+                    <h6>Privacy Policy</h6>
+                  </div>
+                  <div className="ft-tl">
+                    <h6>Terms of Service</h6>
+                  </div>
+                  <div className="ft-tl">
+                    <h6>Disclosur</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     )
 }
 
